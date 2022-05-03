ee40a8863c31e81918581523c4e39b5eb9ebdd9e
VenDiago <alizg@code.edu.az>
2022-05-04 02:39:07 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+window.onload=function () {
+
+    const x =document.getElementById("button")
+
+    function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
